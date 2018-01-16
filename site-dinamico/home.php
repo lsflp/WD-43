ce10cb4d1465ee8335255f2f2715befc615ddba2
Luís Felipe <lipeluiis@hotmail.com>
2018-01-16 20:42:23 -0200

    Adicionando mais códigos das aulas

---
+++ site-dinamico/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="main.css">
+    <title>Responsivo</title>
+</head>
+<body>
+    <div class="site-container">
+       <?php include("inc/header.php"); ?>
+        <main><h1>Home</h1></main>
+        <footer></footer>
+    </div>
+</body>
+</html>
---
+++ site-dinamico/inc/header.php
@@ -0,0 +1,12 @@
+<header>
+    <h1>Meu site</h1>
+    <nav>
+        <ul>
+            <li><a href="home.php">Homepage</a></li>
+            <li><a href="produtos.php">Produtos</a></li>
+            <li><a href="servicos.php">Serviços</a></li>
+            <li><a href="sobre.php">Sobre nós</a></li>
+            <li><a href="contato.php">Contato</a></li>
+        </ul>
+    </nav>
+</header>
