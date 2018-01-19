cbea87a879a69a3e180b2b436ccbb265c8b6b75c
Luís Felipe <lipeluiis@hotmail.com>
2018-01-19 19:25:33 -0200

    Adicionando arquivos do projeto da Mirror Fashion

---
+++ mirror-fashion/checkout.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Checkout Mirror Fashion</title>
+</head>
+<body>
+    <div class="jumbotron">
+        <div class="container">
+            <h1>Ótima escolha!</h1>
+            <p>Obrigado por comprar na Mirror Fashion! 
+            Preencha seus dados para efetivar a compra.</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h2>Sua compra</h2>
+            </div>
+            <div class="panel-body">
+                <img src="img/produtos/foto2-<?= $_POST['cor'] ?>.png" alt="Imagem do produto" class="img-thumbnail img-responsive">
+                <dl>
+                    <dt>Produto</dt>
+                    <dd><?= $_POST['nome'] ?></dd>
+
+                    <dt>Preço</dt>
+                    <dd><?= $_POST['preco'] ?></dd>
+
+                    <dt>Cor</dt>
+                    <dd><?= $_POST['cor'] ?></dd>
+
+                    <dt>Tamanho</dt>
+                    <dd><?= $_POST['tamanho'] ?></dd>
+                    
+                    <dt>Preço</dt>
+                    <dd id="preco">R$ <?= $_POST["preco"] ?></dd>
+                </dl>
+            </div>
+
+            <div class="form-group">
+                <label for="qt">Quantidade</label>
+                <input id="qt" class="form-control" type="number" min="0" max="99" value="1">
+            </div>
+            <div class="form-group">
+                <label for="total">Total</label>
+                <output for="qt preco" id="total" class="form-control">
+                    <?= $_POST["preco"] ?>
+                </output>
+            </div>
+        </div>
+
+        <form action="">
+            <fieldset>
+                <legend>Dados pessoais</legend>
+
+                <div class="form-group">
+                    <label for="nome">Nome completo</label>
+                    <input type="text" name="nome" id="nome" class="form-control" autofocus>
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" class="form-control" placeholder="email@exemplo.com">
+                </div>
+
+                <div class="form-group">
+                    <label for="cpf">CPF</label>
+                    <input type="text" name="cpf" id="cpf" class="form-control" placeholder="000.000.000-00">
+                </div>
+
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="spam" value="sim" checked>
+                        Quero receber spam da Mirror Fashion
+                    </label>
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <legend>Cartão de crédito</legend>
+
+                <div class="form-group">
+                    <label for="numero-cartao">Número - CVV</label>
+                    <input type="text" class="form-control" id="numero-cartao" name="numero-cartao">
+                </div>
+
+                <div class="form-group">
+                    <label for="bandeira-cartao">Bandeira</label>
+                    <select name="bandeira-cartao" id="bandeira-cartao" class="form-control">
+                        <option value="master">MasterCard</option>
+                        <option value="visa">VISA</option>
+                        <option value="amex">American Express</option>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="validade-cartao">Validade</label>
+                    <input type="month" class="form-control" id="validade-cartao" name="validade-cartao">
+                </div>
+            </fieldset>
+
+            <button type="submit" class="btn btn-primary btn-lg pull-right">
+                <span class="glyphicon glyphicon-thumbs-up"></span>
+                Confirmar Pedido
+            </button>
+        </form>
+    </div>
+
+    <script src="js/total.js"></script>
+</body>
+</html>
---
+++ mirror-fashion/index.php
@@ -0,0 +1,166 @@
+<?php 
+    $cabecalho_title="Mirror Fashion";
+    include("cabecalho.php"); 
+?>
+
+    <div class="container destaque">
+        <section class="busca">
+            <h2>Busca</h2>
+
+            <form>
+                <input type="search">
+                <input type="image" src="img/busca.png">
+            </form>
+        </section>
+
+        <section class="menu-departamentos">
+            <h2>Departamentos</h2>
+
+            <nav>
+                <ul>
+                    <li><a href="#">Blusas e Camisas</a>
+                        <ul>
+                            <li><a href="#">Manga curta</a></li>
+                            <li><a href="#">Manga comprida</a></li>
+                            <li><a href="#">Camisa social</a></li>
+                            <li><a href="#">Camisa casual</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Calças</a></li>
+                    <li><a href="#">Saias</a></li>
+                    <li><a href="#">Vestidos</a></li>
+                    <li><a href="#">Sapatos</a></li>
+                    <li><a href="#">Bolsas</a></li>
+                    <li><a href="#">Acessórios  </a></li>
+                </ul>
+            </nav>
+        </section>
+
+        <img src="img/destaque-home.png" alt="Promoção: Big City">
+
+        <div class="container paineis">
+            <section class="painel novidades">
+                <h2>Novidades</h2>
+                <ol>
+
+                    <li>
+                        <a href="produto.html">
+                            <figure>
+                                <img src="img/produtos/miniatura1.png" alt="">
+                                <figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+                            </figure>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="produto.html">
+                            <figure>
+                                <img src="img/produtos/miniatura2.png" alt="">
+                                <figcaption>T-Shirt Skull por R$ 39,90</figcaption>
+                            </figure>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="produto.html">
+                            <figure>
+                                <img src="img/produtos/miniatura3.png" alt="">
+                                <figcaption>Wool Cardigan por R$ 109,90</figcaption>
+                            </figure>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="produto.html">
+                            <figure>
+                                <img src="img/produtos/miniatura4.png" alt="">
+                                <figcaption>Bubble Jacket por R$ 159,90</figcaption>
+                            </figure>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="produto.html">
+                            <figure>
+                                <img src="img/produtos/miniatura5.png" alt="">
+                                <figcaption>Regata Esportiva por R$ 29,90</figcaption>
+                            </figure>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="produto.html">
+                            <figure>
+                                <img src="img/produtos/miniatura9.png" alt="">
+                                <figcaption>Camisa Xadrez por R$ 39,90</figcaption>
+                            </figure>
+                        </a>
+                    </li>
+                </ol>
+            </section>
+
+            <section class="painel mais-vendidos">
+                    <h2>Mais Vendidos</h2>
+                    <ol>
+    
+                        <li>
+                            <a href="produto.html">
+                                <figure>
+                                    <img src="img/produtos/miniatura10.png" alt="">
+                                    <figcaption>Blusa Leve por R$ 129,90</figcaption>
+                                </figure>
+                            </a>
+                        </li>
+    
+                        <li>
+                            <a href="produto.html">
+                                <figure>
+                                    <img src="img/produtos/miniatura11.png" alt="">
+                                    <figcaption>Green Shorts por R$ 49,90</figcaption>
+                                </figure>
+                            </a>
+                        </li>
+    
+                        <li>
+                            <a href="produto.html">
+                                <figure>
+                                    <img src="img/produtos/miniatura12.png" alt="">
+                                    <figcaption>Camisa Leve por R$ 39,90</figcaption>
+                                </figure>
+                            </a>
+                        </li>
+    
+                        <li>
+                            <a href="produto.html">
+                                <figure>
+                                    <img src="img/produtos/miniatura13.png" alt="">
+                                    <figcaption>Camisa Leve por R$ 39,90</figcaption>
+                                </figure>
+                            </a>
+                        </li>
+    
+                        <li>
+                            <a href="produto.html">
+                                <figure>
+                                    <img src="img/produtos/miniatura14.png" alt="">
+                                    <figcaption>Camisa Leve por R$ 39,90</figcaption>
+                                </figure>
+                            </a>
+                        </li>
+    
+                        <li>
+                            <a href="produto.html">
+                                <figure>
+                                    <img src="img/produtos/miniatura15.png" alt="">
+                                    <figcaption>Vestido por R$ 59,90</figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                    </ol>
+                </section>
+            </div>
+        </div>
+
+        <?php include("rodape.php"); ?>
+    </body>
+</html>
